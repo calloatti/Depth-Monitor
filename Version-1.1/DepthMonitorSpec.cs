2c1a797d819ba2e412c46f36cc83ad9aa2d24f2b
Carlos Alloatti <calloatti@gmail.com>
2026-06-30 08:21:13 -0300

    Update: Automated sync on 2026-06-30 08:21
    Automated commit: Changes detected and synchronized via development script.

--- Version-1.1/DepthMonitorSpec.cs
+++ Version-1.1/DepthMonitorSpec.cs
@@ -1,12 +1,9 @@
-﻿using Timberborn.BaseComponentSystem;
-using Timberborn.BlueprintSystem;
+﻿using Timberborn.BlueprintSystem;
 using UnityEngine;
 
 namespace Calloatti.DepthMonitor
 {
-  // Converted from a 'record' inheriting 'ComponentSpec' to a standard 'class' 
-  // inheriting 'BaseComponent' to completely bypass the publicized assembly catch-22.
-  public class DepthMonitorSpec : BaseComponent
+  internal record DepthMonitorSpec : ComponentSpec
   {
     [Serialize]
     public Vector3Int SensorCoordinates { get; init; }
